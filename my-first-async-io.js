b9092673b6f326805013e5e525b1fcad18661be3
Amal-Mansour <amal.mans556@gmail.com>
2021-03-08 22:00:24 +0100

    first commit

---
+++ my-first-async-io.js
@@ -0,0 +1,8 @@
+var fs = require('fs')  
+ let contents =  process.argv[2]    
+
+  fs.readFile(contents,'utf-8',function(err,contents){
+    
+      var lines = contents.toString().split('\n').length - 1  
+      console.log(lines)
+})  
